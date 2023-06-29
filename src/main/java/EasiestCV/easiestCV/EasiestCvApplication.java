cc95cdc98fe2c5bcbe83c820a1ba71bd44791006
SihyeonHong <hongsihyeon179@gmail.com>
2023-06-29 11:57:19 +0900

    [Init] Initial commit

---
+++ src/main/java/EasiestCV/easiestCV/EasiestCvApplication.java
@@ -0,0 +1,13 @@
+package EasiestCV.easiestCV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasiestCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasiestCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EasiestCV/easiestCV/EasiestCvApplicationTests.java
@@ -0,0 +1,13 @@
+package EasiestCV.easiestCV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasiestCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
